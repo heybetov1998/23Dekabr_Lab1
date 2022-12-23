d2c8133555e177508dba6151d57be778b95d47ad
Adil Heybetov <heybetov1998@gmail.com>
2022-12-24 00:53:11 +0400

    added tasks

---
+++ 23Dec_Lab1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _23Dec_Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1 - İkirəqəmli ədədi daxil edirik və ədədin hər bir rəqəmini arada boşluq olaraq ekrana versin
+            //Console.Write("Ikireqemli eded daxil edin: ");
+            //string input = Console.ReadLine();
+
+            //if (input.Length < 2 || input.Length > 2)
+            //{
+            //    Console.WriteLine("Eded iki reqemli deyil");
+            //    return;
+            //}
+
+            //int number = int.Parse(input);
+            //int lastDigit = number % 10;
+            //number /= 10;
+
+            //Console.WriteLine($"{number} {lastDigit}");
+
+            #endregion
+
+            #region Task 2 - 1-dən n-ə qədər tam ədədlərdən 2-ə və 3-ə bölünənlərin cəmini ekrana çıxarsın
+            //Console.Write("Ededi daxil edin: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum = 0;
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    if (i % 2 == 0 & i % 3 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine($"Cem: {sum}");
+            #endregion
+
+            #region Task 3 - a-dan b-yə qədər tam ədədlərin sayını və cəmini ekrana çıxarsın
+            //Console.Write("Baslangic ededi daxil edin: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Son ededi daxil edin: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    sum += i;
+            //    count++;
+            //}
+
+            //Console.WriteLine($"\nEdedlerin sayi: {count}\nEdedlerin cemi: {sum}");
+            #endregion
+        }
+    }
+}
